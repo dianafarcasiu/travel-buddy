4d004f190228f7bed3cd76903a42b1d1d1f53c3d
dianafarcasiu <dianafarcasiu24@gmail.com>
2024-09-23 17:42:21 +0300

    error handling

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ function Logo({ color = "dark" }) {
   return (
     <NavLink to="/">
       <div className="inline-flex items-center logo">
-        <img src={`/logo-${color}.png`} className="w-14 sm:w-16"></img>
+        <img src={`./logo-${color}.png`} className="w-14 sm:w-16"></img>
         <span
           className={`text-2xl sm:text-3xl ${
             color === "dark" ? "text-zinc-800" : "text-amber-500"
