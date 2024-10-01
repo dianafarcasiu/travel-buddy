b2a99bbd1ea77be8746b722392ef209158c70bd7
dianafarcasiu <dianafarcasiu24@gmail.com>
2024-10-01 11:41:51 +0300

    optimized w code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Explore from "./pages/Explore";
+import { CitiesProvider } from "./contexts/CitiesContext";
+
 import CitiesList from "./components/CitiesList";
 import CountriesList from "./components/CountriesList";
 import CityInfo from "./components/CityInfo";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
+import Spinner from "./components/Spinner";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Explore = lazy(() => import("./pages/Explore"));
 
 function App() {
   return (
     <>
       <CitiesProvider>
         <BrowserRouter basename="/travel-buddy">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/explore" element={<Explore />}>
-              <Route
-                index
-                element={<Navigate replace to="/explore/cities" />}
-              />
-              <Route path="cities" element={<CitiesList />} />
-              <Route path="countries" element={<CountriesList />} />
-              <Route path="cities/:id" element={<CityInfo />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-          </Routes>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/explore" element={<Explore />}>
+                <Route
+                  index
+                  element={<Navigate replace to="/explore/cities" />}
+                />
+                <Route path="cities" element={<CitiesList />} />
+                <Route path="countries" element={<CountriesList />} />
+                <Route path="cities/:id" element={<CityInfo />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </>
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,6 +1,6 @@
 function Spinner() {
   return (
-    <div className="flex items-center justify-center mt-20">
+    <div className="flex items-center justify-center mt-[25%]">
       <div className="w-12 h-12 border-4 border-t-transparent border-zinc-400 rounded-full animate-spin"></div>
     </div>
   );
